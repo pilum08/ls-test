56362cec78f186bba58989b0eac89bd48af2cc16
Nikolai Kolotilov <x08042@gamail.com>
2022-06-23 19:43:00 +0500

    create the subtotal component

---
+++ src/components/subtotal/index.js
@@ -0,0 +1,1 @@
+export { default as subtotal } from './subtotal'
---
+++ src/components/subtotal/subtotal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="subtotal">
+        <div class="subtotal__sum">
+            <span class="subtotal__sum-text">Промежуточный итог по корзине:</span>
+            <span class="subtotal__sum-num">{{subtotalSum}}</span>
+            <span class="subtotal__currency">р</span>
+        </div>
+        <div class="subtotal__tax">
+            <span class="subtotal__tax-text">В том числе НДС:</span>
+            <span class="subtotal__tax-num">{{subtotalTaxSum}}</span>
+            <span class="subtotal__currency">р</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  props: [
+    'subtotalSum',
+    'subtotalTaxSum'
+  ]
+}
+</script>
+<style lang="scss" src="./subtotal.scss" scoped>
+
+</style>
