d7fc2ada83ef4790c1d75d489ac3596e99f3dd90
Nikolai Kolotilov <x08042@gamail.com>
2022-06-23 19:55:59 +0500

    create the total component

---
+++ src/components/total/index.js
@@ -0,0 +1,1 @@
+export { default as total } from './total'
---
+++ src/components/total/total.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="total">
+        <span class="total__text">итого:</span>
+        <span class="total__num">{{totalNum}}</span>
+    </div>
+</template>
+<script>
+export default {
+  props: ['totalNum']
+}
+</script>
+<style src="./total.scss" lang="scss" scoped>
+
+</style>
